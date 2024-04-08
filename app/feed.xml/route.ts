a28e16be4b6912ae8fcb1af225ed82de71b8af77
arcataroger <arcataroger@gmail.com>
2024-04-08 14:57:34 -0700

    Fix validation errors

--- app/feed.xml/route.ts
+++ app/feed.xml/route.ts
@@ -1,47 +1,38 @@
 import {performRequest} from "../../lib/datocms";
 import {Feed} from "feed";
+import {Enclosure} from "feed/lib/typings";
 
 
 export async function GET() {
+
+    // Root of your website
+    const baseUrl = 'https://forum-4976-nextjs-blog-rss-feed.vercel.app/'
+
     const {allPosts, _site} = await performRequest({
         query: RSS_QUERY, // Your Dato query
         revalidate: 60 // in seconds
     })
 
-    /* Example of allPosts
-    [
-      {
-        "id": "19072916",
-        "slug": "unforgettable-trip-to-the-great-wall-in-china",
-        "title": "Unforgettable Trip to The Great Wall in China",
-        "coverImage": {
-          "responsiveImage": {
-            "src": "https://www.datocms-assets.com/128930/1585207017-image-30-copyright.jpg?h=500&w=500"
-          }
-        },
-        "date": "2020-03-06",
-        "excerpt": "Our recommendations for your trip to the Great Wall in China."
-      },
-      etc.
-    ]
-     */
-
     const feed = new Feed({
         title: 'RSS Example with DatoCMS and Next.js',
         description: "Using DatoCMS and Next.js with the Feed lib to generate a RSS feed",
-        id: "https://forum-4976-nextjs-blog-rss-feed.vercel.app/",
-        link: "https://forum-4976-nextjs-blog-rss-feed.vercel.app/",
+        id: baseUrl,
+        link: baseUrl,
         language: "en", // optional, used only in RSS 2.0, possible values: http://www.w3.org/TR/REC-html40/struct/dirlang.html#langcodes
-        image: allPosts[0]?.coverImage?.responsiveImage?.src ?? undefined,
+        image: allPosts[0].coverImage.responsiveImage.src ? encodeURI(allPosts[0].coverImage.responsiveImage.src) : undefined,
         favicon: _site.favicon?.url ?? undefined,
         copyright: "All rights reserved",
     })
 
     allPosts.forEach(post => {
+        const postUrl = new URL(`/posts/${post.slug}`, baseUrl).toString();
+        const imageUrl =  post.coverImage.responsiveImage.src ? new URL(post.coverImage.responsiveImage.src): undefined;
+        const imageUrlEncoded = imageUrl && `${imageUrl.origin}${imageUrl.pathname}${encodeURIComponent(imageUrl.search)}`
+
         feed.addItem({
             title: post.title,
-            id: post.id,
-            link: `/${post.slug}`,
+            id: postUrl,
+            link: postUrl,
             description: post.excerpt,
             author: [
                 {
@@ -49,7 +40,7 @@ export async function GET() {
                 },
             ],
             date: new Date(post.date),
-            image: post.coverImage?.responsiveImage?.src ?? undefined
+            image: imageUrlEncoded,
         });
     });
 
